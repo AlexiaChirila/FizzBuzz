9221e2740cc84a0646e5738ae395f75c1ce2ba77
AlexiaChirila <Alexia.Chirila@softwire.com>
2026-07-08 14:49:24 +0300

    Added Functions for user choice of rules from console

--- src/index.ts
+++ src/index.ts
@@ -92,6 +92,36 @@ function fizzbuzzMaximumNumber(number:number): void {
     }
 }
 
+function fizzbuzzChooseRule(numberArray:number[]): void {
+    for (let i = 1; i <= 256; i++)
+    {
+        const words: string[] = [];
+        if (i % 11 === 0) {
+            words.push("Bong");
+        } else for (const [number, phrase] of rules) {
+            if (numberArray.includes(number) && i % number === 0) {
+                words.push(phrase);
+            }
+        }
+
+        if (numberArray.includes(13) && i % 13 === 0) {
+            const index = words.findIndex(word => word.startsWith("B"));
+            if (index === -1) {
+                words.push("Fezz");
+            } else {
+                words.splice(index, 0, "Fezz");
+            }
+        }
+        if (i % 17 === 0) {
+            words.reverse();
+        }
+
+        console.log(words.length ? words.join('') : i);
+
+    }
+}
+
+
 //fizzbuzzPart1();
 //fizzbuzz2();
 
@@ -100,13 +130,13 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function askForNumber(prompt: string): void {
+/*function askForNumber(prompt: string): void {
     rl.question(prompt, (input: string) => {
 
         const num = Number(input.trim());
         if (Number.isNaN(num)) {
             console.log("Invalid input. Please enter a valid number.");
-            askForNumber(prompt); // Ask again
+            askForNumber(prompt);
         } else {
             fizzbuzzMaximumNumber(num);
             rl.close();
@@ -115,3 +145,36 @@ function askForNumber(prompt: string): void {
 }
 
 askForNumber("Enter a number: ");
+
+ */
+
+function checkForAvailableRules(num:number): boolean {
+
+        return !(num != 3 && num !== 5 && num !== 13);
+}
+
+function askForRules(prompt: string): void {
+    rl.question(prompt, (input: string) => {
+        try {
+
+            const numArray: number[] = input
+                .split(",")
+                .map(v => v.trim())
+                .filter(v => v.length > 0)
+                .map(v => {
+                    const num = Number(v);
+                    if (isNaN(num) || !checkForAvailableRules(num)) {
+                        throw new Error(`Invalid rule: "${v}"`);
+                    }
+                    return num;
+                });
+            fizzbuzzChooseRule(numArray);
+        }catch (err) {
+        console.error("Error:", (err as Error).message);
+    } finally {
+        rl.close();
+    }
+    });
+}
+
+askForRules("Please choose the rules separated by comma that you want to apply (3,5,13): ");
