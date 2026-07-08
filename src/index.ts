bcced548d166b28d8445ef2ab0846d383a6dfdc9
AlexiaChirila <Alexia.Chirila@softwire.com>
2026-07-08 15:36:38 +0300

    Refactored function

--- src/index.ts
+++ src/index.ts
@@ -40,25 +40,7 @@ export function applyRules(num: number, applyRules:number[]):string {
 }
 
 function fizzbuzzPart1(): void {
-    for (let i=1; i<=100; i++)
-    {
-        if(i%5===0 && i%3===0)
-        {
-            console.log("FizzBuzz");
-        }
-        else if(i%5===0)
-        {
-            console.log("Buzz");
-        }
-        else if(i%3===0)
-        {
-            console.log("Fizz");
-        }
-        else
-        {
-            console.log(i);
-        }
-    }
+    for (let i=1; i<=100; i++) applyRules(i,[3,5]);
 }
 
 function fizzbuzz2(): void {
